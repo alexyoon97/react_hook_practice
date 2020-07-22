0ae0b5cdd71cdf1b67d890574d2a5ed97f22d81c
Alex Yoon <wonsangalex@gmail.com>
2020-07-22 00:20:59 -0700

    hook - useInput

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import useInput from './hooks/useInput'
 
 function App() {
+  const [number, setNumber] = useState(1);
+  const increaseNumber = () => {setNumber(number + 1)}
+  const decreaseNumber = () => {setNumber(number - 1)}
+  const maxLen = value => value.length <= number
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="container">
-      <div className="hook_div">
-        <div className="text">Response</div>
-        <button className="btn_hook">Click</button>
+      <div className="hook_div"> 
+        <input className="text" placeholder="use input hook" {...name}/><br></br>
+        Max Length: {number} <button onClick={increaseNumber}>Up</button><button onClick={decreaseNumber}>Down</button>
       </div>
       <div className="hook_div">
         <div className="text">Response</div>
